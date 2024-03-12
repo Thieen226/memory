e73cc6063932b0c5c2fab4901e8140fe3bed2370
Thieen226 <t.ho1@my.metroed.net>
2024-03-12 10:28:03 -0700

    add

--- memoryMatch/memoryMatch/ContentView.swift
+++ memoryMatch/memoryMatch/ContentView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationView{
+                ZStack {
+                    Image("StartViewBackground")
+                    VStack{
+                        Text("EmojiMatch")
+                            .padding()
+                            .background(.green)
+                       
+                        NavigationLink(destination: gameView()) {
+                            Text("Start")
+                                .padding()
+                                .background(.blue)
+                                .foregroundColor(.white)
+                        }
+                    }
+            }
         }
-        .padding()
     }
 }
 
+struct GameView: View{
+    @State var cardsFlipped: [Bool] = Array(repeating: false, count: 16)
+
+    @State var emojis = ["😀", "😀" ,"😁", "😁", "😂", "😂", "🤣", "🤣", "😃", "😃", "😄", "😄",]
+
+    @State private var pickOne: Int = -1
+
+    @State private var pickTwo: Int = -1
+
+    @State private var score: Int = 0
+
+    @State private var gameFinished: Bool = false
+    
+    var body: some View{
+        VStack{
+            
+        }
+    }
+}
 #Preview {
     ContentView()
 }
---
+++ memoryMatch/memoryMatch/gameView.swift
@@ -0,0 +1,19 @@
+//
+//  gameView.swift
+//  memoryMatch
+//
+//  Created by StudentAM on 3/12/24.
+//
+
+import SwiftUI
+
+struct gameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+          
+    }
+}
+
+#Preview {
+    gameView()
+}
