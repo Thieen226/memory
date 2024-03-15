b6f8c3082133901fea87f121655430c1f515054c
Thieen226 <t.ho1@my.metroed.net>
2024-03-15 10:27:56 -0700

    add

--- memoryMatch/memoryMatch/gameView.swift
+++ memoryMatch/memoryMatch/gameView.swift
@@ -21,24 +21,45 @@ struct gameView: View {
     @State private var gameFinished: Bool = false
     
     let columns = [
-        GridItem(.fixed(50), spacing: nil, alignment: nil),
-        GridItem(.fixed(50), spacing: nil, alignment: nil),
-        GridItem(.fixed(50), spacing: nil, alignment: nil)
+        GridItem(.fixed(120), spacing: nil, alignment: nil),
+        GridItem(.fixed(120), spacing: nil, alignment: nil),
+        GridItem(.fixed(120), spacing: nil, alignment: nil)
     ]
     
     var body: some View {
-        VStack{
-            Text("Current Score \(score)")
-                .padding()
-            
-            LazyVGrid(columns: columns, spacing: 10) {
-                ForEach(emojis.indices, id: \.self){ item in Text("\(emojis[item])")}
+        NavigationView{
+            ZStack{
+                Image("gameViewBackground")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack{
+                    Text("Current Score: \(score)")
+                        .padding()
+                        .font(Font.custom("MadimiOne-Regular", size:
+                                         45))
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach(emojis.indices, id: \.self){ item in Text(emojis[item])}
+                            .font(.system(size: 40))
+                            .frame(width: 90, height: 90)
+                            .background(.blue)
+                            .cornerRadius(6)
+                            .onTapGesture {
+                                
+                            }
+                        }
+                    }
+                }
             }
-        }
         .navigationBarBackButtonHidden(true)
-       
-          
-    }
+            
+        }
+        
+    
+    
+//    struct manageCards: View {
+//        
+//    }
     
 }
 
